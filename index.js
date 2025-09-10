8c74fe5e13b7933c5f43ad52ff6718de31877280
FeeehSoares <felipestyte@gmai.com>
2025-09-09 22:18:32 -0300

    versao inicial

---
+++ index.js
@@ -0,0 +1,8 @@
+var bruna = window.document.getElementById("bruna")
+var Leonardo = window.document.getElementById("Leonardo")
+var Samantha = window.document.getElementById("Samantha")
+
+function rolarpradireita() {
+    bruna.style = "display;none"
+    Samantha.style = "display;flex"
+}
